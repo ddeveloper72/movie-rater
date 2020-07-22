415a2e23c8e0c4ff592aead58087e0ad7fe06e07
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-23 00:11:31 +0100

    added imports for main module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AuthModule } from './auth/auth.module';
+import { MainModule } from './main/main.module';
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AuthModule
+    AuthModule,
+    MainModule
   ],
   imports: [
     BrowserModule
