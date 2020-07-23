b76372940d653ddd617f79b7ca451e78301c06ec
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-23 23:41:12 +0100

    added movie-list componets (--module=main)

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main.component';
+import { MovieListComponent } from '../movie-list/movie-list.component';
 
 // declare a name for the array of routes
 const routes: Routes = [
@@ -12,7 +13,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [MainComponent],
+  declarations: [MainComponent, MovieListComponent],
   imports: [
     CommonModule,
     // added imports for routes
