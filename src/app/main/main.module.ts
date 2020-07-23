50a452d91db2b2cbdfba3fb0c58b9f199ec9488c
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-23 23:10:38 +0100

    added array declearatiosn for the routes

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -4,6 +4,8 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { MainComponent } from './main.component';
 
+const routes: Routes = [];  // declare a name for the array of routes
+
 
 
 @NgModule({
