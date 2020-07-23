2c35026114a0ea088733f625086b4e131213d49b
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-23 23:43:25 +0100

    added movie-details components (--module=main)

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,6 +5,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main.component';
 import { MovieListComponent } from '../movie-list/movie-list.component';
 import { MovieFormComponent } from '../movie-form/movie-form.component';
+import { MovieDetailsComponent } from '../movie-details/movie-details.component';
 
 // declare a name for the array of routes
 const routes: Routes = [
@@ -14,7 +15,7 @@ const routes: Routes = [
 
 
 @NgModule({
-  declarations: [MainComponent, MovieListComponent, MovieFormComponent],
+  declarations: [MainComponent, MovieListComponent, MovieFormComponent, MovieDetailsComponent],
   imports: [
     CommonModule,
     // added imports for routes
