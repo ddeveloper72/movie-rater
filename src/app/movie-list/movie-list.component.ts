5e8b5b4e2076984c6e2c8c42098c82215d5cb425
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-25 16:44:04 +0100

    add method for moviecCicked WIP

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
@@ -14,5 +14,7 @@ export class MovieListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {}
+
+  movieClicked() {}
 
 }
