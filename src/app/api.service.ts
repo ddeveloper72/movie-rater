96fc873556a2f8cef05b705c512549eaf480e873
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-25 03:53:37 +0100

    corrected typose in imports

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClinet, Httpheaders, HttpClient } from '@angular/common/http';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
