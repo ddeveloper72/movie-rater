1ec7df5f63b0a2e918e3708bd67da7d56b3add5d
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-25 05:05:44 +0100

    added token authentiation to baseUrl header

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,18 +1,22 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
   baseUrl = 'http://127.0.0.1:8000/api/movies/';
+  headers = new HttpHeaders({
+    'Content-Type:': 'application/json',
+    Authorization: environment.Token
+  });
 
   constructor(
     private httpClient: HttpClient // initialize the HttpClient
   ) {}
 
   getMovies() {
-    return this.httpClient.get(this.baseUrl);
+    return this.httpClient.get(this.baseUrl, {headers: this.headers});  // pass headers to baseUrl
   }
 }
