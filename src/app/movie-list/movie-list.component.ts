79c6f81dffb095b8abe8627ff078aee1459435b6
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-25 16:36:00 +0100

    make movies accessible as an imput from the main parent component

--- src/app/movie-list/movie-list.component.ts
+++ src/app/movie-list/movie-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 
 @Component({
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MovieListComponent implements OnInit {
 
+  @Input()
   movies: any = [];  // list component now comes from api service, type of any
 
   constructor() { }
