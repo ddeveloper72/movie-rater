11e40eca9a608655836823be7d3cc940a8506375
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-25 03:40:50 +0100

    add base url for api

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -4,6 +4,9 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ApiService {
+  
+  baseUrl = 'http://127.0.0.1:8000/api/movies/';
+
   private movies = ['Terminator', 'Predator']; // mockup api data from a service
 
   constructor() {}
