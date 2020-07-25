3373d0fff0fae778bf232a95dd4fb971a38a321b
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-25 04:00:47 +0100

    refactor the construction api to return data from the baseUrl

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -8,15 +8,11 @@ export class ApiService {
 
   baseUrl = 'http://127.0.0.1:8000/api/movies/';
 
-  private movies = ['Terminator', 'Predator']; // mockup api data from a service
-
   constructor(
     private httpClient: HttpClient // initialize the HttpClient
   ) {}
 
   getMovies() {
-    const dynamicMovies = this.httpClient.get(this.baseUrl);
-    console.log(dynamicMovies);
-    return this.movies;
+    return this.httpClient.get(this.baseUrl);
   }
 }
