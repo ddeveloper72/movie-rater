c58a221b01081f14027f231b671aee95b347e662
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-25 03:43:01 +0100

    added inports fro angular common http

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
+import { HttpClinet, Httpheaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  
+
   baseUrl = 'http://127.0.0.1:8000/api/movies/';
 
   private movies = ['Terminator', 'Predator']; // mockup api data from a service
