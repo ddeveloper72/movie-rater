ebbc659bd5982ba4294d7cbbfd5640df15a5924a
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-26 22:41:12 +0100

    Revised inputy type for movie default any type

--- src/app/movie-details/movie-details.component.ts
+++ src/app/movie-details/movie-details.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MovieDetailsComponent implements OnInit {
 
-  @Input() movie: string;
+  @Input() movie;
 
   constructor() { }
 
