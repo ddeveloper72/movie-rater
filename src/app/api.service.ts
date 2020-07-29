20b30f4db716397e4c91897e8f03b1640f8dd4e4
ddeveloper72 <duncan.niamh@gmail.com>
2020-07-29 22:23:30 +0100

    specify the data types for thge base urls from the Movie model

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
+import { Movie } from './models/Movie';
 
 @Injectable({
   providedIn: 'root'
@@ -17,18 +18,18 @@ export class ApiService {
   ) {}
 
   getMovies() {
-    return this.httpClient.get(this.baseUrl, {headers: this.headers});  // pass headers to baseUrl
+    return this.httpClient.get<Movie>(this.baseUrl, {headers: this.headers});  // pass headers to baseUrl
   }
 
 
   // get the specific rating of a movie so the data can be refreshed dynamically after user adds rating
   getMovie(id: number) {
-    return this.httpClient.get(`${this.baseUrl}${id}/`, { headers: this.headers });  // pass headers to baseUrl
+    return this.httpClient.get<Movie>(`${this.baseUrl}${id}/`, { headers: this.headers });  // pass headers to baseUrl
   }
 
   rateMovie(rate: number, movieId: number) {
     const body = JSON.stringify({stars: rate});  // information from the movie rated
-    return this.httpClient.post(`${this.baseUrl}${movieId}/rate_movie/`, body, {
+    return this.httpClient.post<Movie>(`${this.baseUrl}${movieId}/rate_movie/`, body, {
       headers: this.headers
     });  // pass headers to baseUrl
   }
