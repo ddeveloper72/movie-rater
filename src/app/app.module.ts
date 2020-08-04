9041eb187ac7eac4be6e73cd7a46cf4df1e54c9e
ddeveloper72 <duncan.niamh@gmail.com>
2020-08-04 19:10:29 +0100

    added imports for ReactiveFormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { IconSpriteModule } from 'ng-svg-icon-sprite';
 
 import { AuthModule } from './auth/auth.module';
@@ -20,6 +21,7 @@ const routes: Routes = [
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule,
     AuthModule,
     MainModule,
     HttpClientModule,
