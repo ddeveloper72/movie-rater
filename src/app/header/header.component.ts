1883a3ef105f718ad09d6f11c655901f4632c888
ddeveloper72 <duncan.niamh@gmail.com>
2020-08-11 21:50:54 +0100

    emit logoutUser to app component to logout user

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-
 export class HeaderComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  @Output() logoutUser = new EventEmitter();
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onLogout(): void {
+    this.logoutUser.emit();
   }
-
 }
