e4da1abe720ae30b0cce10ed21cc5d6689e6be61
ddeveloper72 <duncan.niamh@gmail.com>
2020-08-12 18:47:13 +0100

    removed api get mocies service, which has to remain in main component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Movie } from './models/Movie';
 import { ApiService } from './api.service';
 import { CookieService } from 'ngx-cookie-service';
 
@@ -10,7 +9,6 @@ import { CookieService } from 'ngx-cookie-service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  movies: Movie[] = []; // list component now comes from api service, type of any
 
   constructor(
     private apiService: ApiService, // initialize service
@@ -20,22 +18,10 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     const mrToken = this.cookieService.get('mr-token'); // get the token at ngOnInit stage & store as constant
-
-    // redirect if no token to auth, else if token, show movies
-    if (!mrToken) {
-      this.router.navigate(['/auth']);
-    } else {
-      this.apiService.getMovies().subscribe(
-        (data: Movie[]) => {
-          this.movies = data;
-        },
-        error => console.error()
-      );
-    }
   }
 
   logoutUser(): void {
     this.cookieService.delete('mr-token');
-    this.router.navigate(['/auth']);
+    this.router.navigate(['/home']);
   }
 }
