92c17b58d6f916b0759794c3bed52320215456f3
ddeveloper72 <duncan.niamh@gmail.com>
2020-08-16 10:48:28 +0100

    wire in valid cookie method and emit to header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input} from '@angular/core';
 import { Router } from '@angular/router';
 import { ApiService } from './api.service';
 import { CookieService } from 'ngx-cookie-service';
@@ -10,18 +10,31 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class AppComponent implements OnInit {
 
+  // send status of mrToken to header components
+  @Output() validToken = new EventEmitter();
+
+
   constructor(
     private apiService: ApiService, // initialize service
     private cookieService: CookieService,
     private router: Router
   ) {}
 
-  ngOnInit(): void {
+
+  ngOnInit(): void {}
+
+  // check if mrToken is present in the cookieService
+  validCookie(): void {
     const mrToken = this.cookieService.get('mr-token'); // get the token at ngOnInit stage & store as constant
+    if (mrToken) {
+      this.validToken.emit();
+      console.log('✔ Valid Token:', mrToken);
+    }
   }
 
+  // remove mrToken if logout event received
   logoutUser(): void {
     this.cookieService.delete('mr-token');
-    this.router.navigate(['/home']);
+    this.router.navigate(['/']);
   }
 }
