fafa35cfd25ccd13476400fbcec8c7bea7900578
ddeveloper72 <duncan.niamh@gmail.com>
2020-08-16 10:44:40 +0100

    wiring in is authenticated method to app component

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,12 +6,26 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+
+
   constructor() {}
 
+  // retrieve validToken passed from app component
+  @Input() validToken: any;
+  // pass logoutUser to app component
   @Output() logoutUser = new EventEmitter();
 
   ngOnInit(): void {}
 
+  // if token valid, pass to markup register link
+  isAuthenticated(): void {
+    const mrToken = this.validToken();
+    if (mrToken) {
+      this.validToken = true;
+    }
+  }
+
+  // if click event, pass to app component
   onLogout(): void {
     this.logoutUser.emit();
   }
