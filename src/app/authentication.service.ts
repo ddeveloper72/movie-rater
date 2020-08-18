ca5a73361d2703cb0c65749c63bb26027c22e0d6
ddeveloper72 <duncan.niamh@gmail.com>
2020-08-18 19:39:27 +0100

    added user model

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+import { User } from './models/User';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private currentUserSubject: BehaviorSubject<User>;
+  public currentUser: Observable<User>;
 
-  constructor() { }
+  constructor() {}
 }
