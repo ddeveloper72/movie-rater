0441f99dc8eca8956023769e105b22540c97f94f
ddeveloper72 <duncan.niamh@gmail.com>
2020-08-18 19:55:30 +0100

    define the user's values in the current user subject as public

--- src/app/authentication.service.ts
+++ src/app/authentication.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
 
 import { User } from './models/User';
@@ -10,5 +11,18 @@ export class AuthenticationService {
   private currentUserSubject: BehaviorSubject<User>;
   public currentUser: Observable<User>;
 
-  constructor() {}
+  constructor(private http: HttpClient) {
+
+    // BehaviorSubject is a special type of Subject that keeps hold of the current
+    // value and emits it to any new subscribers as soon as they subscribe.
+    // This is being used over ngx-cookie-service
+    this.currentUserSubject = new BehaviorSubject<User>(
+      JSON.parse(localStorage.getItem('currentUser'))
+    );
+    this.currentUser = this.currentUserSubject.asObservable();
+  }
+
+  public get currentUserValue(): User {
+    return this.currentUserSubject.value;
+  }
 }
