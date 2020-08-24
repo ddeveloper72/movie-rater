09c3d898b525be5c2f17d88203cc9420f753ad17
ddeveloper72 <duncan.niamh@gmail.com>
2020-08-24 18:58:25 +0100

    configured api service to work with new AuthService removing CookieService

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { CookieService } from 'ngx-cookie-service';
-import { environment } from '../environments/environment';  // used during dev for storing token
 import { Movie } from './models/Movie';
-
+import { AuthenticationService } from './services/authentication.service';
 @Injectable({
   providedIn: 'root'
 })
@@ -16,39 +14,39 @@ export class ApiService {
 
   constructor(
     private httpClient: HttpClient, // initialize the HttpClient
-    private cookieService: CookieService  // initialize the service
+    public authenticationService: AuthenticationService
   ) {}
 
   getMovies() {
     return this.httpClient.get<Movie[]>(this.baseMovieUrl, {
-      headers: this.getAuthHeaders()
+      headers: this.authenticationService.getAuthHeaders()
     }); // pass headers to baseMovieUrl
   }
 
   // get the specific rating of a movie so the data can be refreshed dynamically after user adds rating
   getMovie(id: number) {
     return this.httpClient.get<Movie>(`${this.baseMovieUrl}${id}/`, {
-      headers: this.getAuthHeaders()
+      headers: this.authenticationService.getAuthHeaders()
     }); // pass headers to baseMovieUrl
   }
 
   createMovie(title: string, description: string) {
     const body = JSON.stringify({ title, description }); // convert JSON object to string
     return this.httpClient.post(`${this.baseMovieUrl}`, body, {
-      headers: this.getAuthHeaders()
+      headers: this.authenticationService.getAuthHeaders()
     }); // add new body to the url
   }
 
   updateMovie(id: number, title: string, description: string) {
     const body = JSON.stringify({ title, description }); // convert JSON object to string
     return this.httpClient.put(`${this.baseMovieUrl}${id}/`, body, {
-      headers: this.getAuthHeaders()
+      headers: this.authenticationService.getAuthHeaders()
     }); // add new body to the url
   }
 
   deleteMovie(id: number) {
     return this.httpClient.delete(`${this.baseMovieUrl}${id}/`, {
-      headers: this.getAuthHeaders()
+      headers: this.authenticationService.getAuthHeaders()
     }); // add new body to the url
   }
 
@@ -58,23 +56,8 @@ export class ApiService {
       `${this.baseMovieUrl}${movieId}/rate_movie/`,
       body,
       {
-        headers: this.getAuthHeaders()
+        headers: this.authenticationService.getAuthHeaders()
       }
     ); // pass headers to baseMovieUrl
-  }
-
-  loginUser(authData) {
-    const body = JSON.stringify(authData); // convert JSON object to string
-    return this.httpClient.post(`${this.baseUrl}auth/`, body, {
-      headers: this.getAuthHeaders()
-    });
-  }
-
-  getAuthHeaders() {
-    const token = this.cookieService.get('mr-token');  // define source of the token, ie from the service
-    return new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization: `Token ${token}` // use reference the token from cookie service
-    });
   }
 }
