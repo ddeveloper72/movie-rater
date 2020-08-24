f22af8d4663815545693623748a6a2c57611e5ca
ddeveloper72 <duncan.niamh@gmail.com>
2020-08-24 21:44:03 +0100

    removed implements onInit whihc will be used again later

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { AuthenticationService } from '../services/authentication.service';
@@ -9,7 +9,7 @@ import { AuthenticationService } from '../services/authentication.service';
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
   isAuthenticated = false;
 
   constructor(
