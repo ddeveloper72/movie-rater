cb3d62630e4868a35fb9c5a52869b89ce35e2ab9
ddeveloper72 <duncan.niamh@gmail.com>
2020-08-24 19:07:51 +0100

    configured auth component to use authenticationService without cookieService

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { CookieService } from 'ngx-cookie-service';
-import { ApiService } from '../api.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router, ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from '../helper/must-match.validator';
 
 // define the data type fo the token going to be used
 interface TokenObject {
@@ -14,36 +15,84 @@ interface TokenObject {
   styleUrls: ['./auth.component.css']
 })
 export class AuthComponent implements OnInit {
-  authForm = new FormGroup({
-    username: new FormControl('',
-    [Validators.required]
-    ),
-    password: new FormControl('',
-    [Validators.required]
-    )
-  });
+  authForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = '';
+  registerMode = false;
 
   constructor(
-    private apiService: ApiService,
-    private cookieService: CookieService,
-    private router: Router
-  ) {}
+    private formBuilder: FormBuilder,
+    public authenticationService: AuthenticationService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {
+    // redirect to movies if already logged in
+    if (this.authenticationService.currentUserValue) {
+      this.router.navigate(['/auth']);
+    }
+  }
 
   ngOnInit(): void {
-    const mrToken = this.cookieService.get('mr-token');  // get the token at ngOnInit stage & store as constant
-    if (mrToken) {
-      this.router.navigate(['/movies']);
-    }
-    console.log('😎 Mr Token', mrToken);
+
+    this.authForm = this.formBuilder.group(
+      {
+        username: ['', Validators.required],
+        password: ['', [Validators.required, Validators.minLength(6)]],
+        confirmPassword: ['', Validators.required]
+      },
+      {
+        validator: MustMatch('password', 'confirmPassword')
+      }
+    );
+
+    // get return url from route parameters or default to '/'
+    // this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/auth';
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.authForm.controls;
   }
 
   saveForm(): void {
-    this.apiService.loginUser(this.authForm.value).subscribe(
-      (result: TokenObject) => {  // use the data type definition for what the token is
-        this.cookieService.set('mr-token', result.token);  //  define the name of the token file and result of what it is to contain
+    this.submitted = true;
+    if (this.authForm.invalid) {
+      return;
+    }
+    if (!this.registerMode) {
+      this.loginUser();
+    } else {
+      this.authenticationService.register(this.authForm.value).subscribe(
+        result => {
+          this.loginUser();
+          // save the authentication token from the backend as a TokenObject in local storage
+          console.log('Register form result:', result);
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        }
+      );
+    }
+}
+
+
+loginUser() {
+  if (!this.authForm.invalid && !this.registerMode) {
+    this.authenticationService.login(this.authForm.value).subscribe(
+      (result: TokenObject) => {
+        // pass data to AuthenticationService service where it is saved in local storage as a value.
+        // the AuthService then shares the user status with other app components.
         this.router.navigate(['/movies']);
       },
-      error => console.log(error)
+      error => {
+        this.error = error;
+        this.loading = false;
+      }
     );
   }
- }
+
+  }
+}
