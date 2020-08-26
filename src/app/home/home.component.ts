35616b23a61a39f4710e3b26df83b8fc31740a30
ddeveloper72 <duncan.niamh@gmail.com>
2020-08-27 00:12:39 +0100

    added method to getUsers from ApiService for testing during app build

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { first } from 'rxjs/operators';
+
+import { User } from '../models/user';
+import { ApiService } from '../services/api.service';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  loading = false;
+  users: User[];
 
-  constructor() { }
+  constructor(private apiService: ApiService) {}
 
+  // added method for retrieving all users from api db for testing purposes
   ngOnInit(): void {
+    this.loading = true;
+    this.apiService
+      .getUsers()
+      .pipe(first())
+      .subscribe(users => {
+        this.loading = false;
+        this.users = users;
+      });
   }
-
 }
