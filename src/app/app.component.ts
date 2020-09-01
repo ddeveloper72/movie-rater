c392a5ec1f5ce0d78df9c27d3db91ebd8ed01a71
ddeveloper72 <duncan.niamh@gmail.com>
2020-09-01 22:54:06 +0100

    removed all cookie validation methods from app components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,40 +1,16 @@
-import { Component, OnInit, EventEmitter, Output, Input} from '@angular/core';
-import { Router } from '@angular/router';
-import { ApiService } from './api.service';
-import { CookieService } from 'ngx-cookie-service';
+import { Component } from '@angular/core';
+// import { Router } from '@angular/router';
+// import { User } from './models/User';
+// import { AuthenticationService } from './services/authentication.service';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
+  // currentUser: User;
 
-  // send status of mrToken to header components
-  @Output() validToken = new EventEmitter();
-
-
-  constructor(
-    private apiService: ApiService, // initialize service
-    private cookieService: CookieService,
-    private router: Router
-  ) {}
-
-
-  ngOnInit(): void {}
-
-  // check if mrToken is present in the cookieService
-  validCookie(): void {
-    const mrToken = this.cookieService.get('mr-token'); // get the token at ngOnInit stage & store as constant
-    if (mrToken) {
-      this.validToken.emit();
-      console.log('✔ Valid Token:', mrToken);
-    }
-  }
-
-  // remove mrToken if logout event received
-  logoutUser(): void {
-    this.cookieService.delete('mr-token');
-    this.router.navigate(['/']);
-  }
+  constructor() {}
 }
