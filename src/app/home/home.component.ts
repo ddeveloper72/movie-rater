2bed465cd0dc31ce1b69adf457d4d3211a836b03
ddeveloper72 <duncan.niamh@gmail.com>
2020-09-01 23:00:57 +0100

    added user authentication validation for displaying api data

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 
-import { User } from '../models/user';
+import { User } from '../models/User';
+import { AuthenticationService } from '../services/authentication.service';
 import { ApiService } from '../services/api.service';
 
 @Component({
@@ -10,10 +11,16 @@ import { ApiService } from '../services/api.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  currentUser: User;
   loading = false;
   users: User[];
 
-  constructor(private apiService: ApiService) {}
+  constructor(
+    private apiService: ApiService,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+  }
 
   // added method for retrieving all users from api db for testing purposes
   ngOnInit(): void {
