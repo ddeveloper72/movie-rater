a6d877529877e2f41b139675fce07398d8cd869f
ddeveloper72 <duncan.niamh@gmail.com>
2020-09-03 23:52:20 +0100

    updated app routing  auth component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,12 @@
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { MainComponent } from './main/main.component';
 import { AuthComponent } from './auth/auth.component';
 import { AuthGuard } from './helper/auth.guard';
 
 const routes: Routes = [
     { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: AuthComponent },
+    { path: 'auth', component: AuthComponent },
 
     // otherwise redirect to home
     { path: '**', redirectTo: '' }
