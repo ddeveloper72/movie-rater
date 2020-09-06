c54136bef95cd12892b052cdbbfc9a2f82ea14ee
ddeveloper72 <duncan.niamh@gmail.com>
2020-09-06 17:32:06 +0100

    added password must match methods for auth form

---
+++ src/app/helper/must-match.validator.ts
@@ -0,0 +1,21 @@
+import { FormGroup } from '@angular/forms';
+
+// custom validator to check that two fields match
+export function MustMatch(controlName: string, matchingControlName: string) {
+  return (authForm: FormGroup) => {
+    const control = authForm.controls[controlName];
+    const matchingControl = authForm.controls[matchingControlName];
+
+    if (matchingControl.errors && !matchingControl.errors.mustMatch) {
+      // return if another validator has already found an error on the matchingControl
+      return;
+    }
+
+    // set error on matchingControl if validation fails
+    if (control.value !== matchingControl.value) {
+      matchingControl.setErrors({ mustMatch: true });
+    } else {
+      matchingControl.setErrors(null);
+    }
+  };
+}
