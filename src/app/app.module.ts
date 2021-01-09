08e935c268a1d1d4c32e116c94e2f5b45ec896d3
ddeveloper72 <duncan.niamh@gmail.com>
2021-01-09 12:08:54 +0000

    added imports for shared component module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { IconSpriteModule } from 'ng-svg-icon-sprite';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
 import { HttpErrorInterceptor } from './helper/error.interceptor';
-import { LoadingSpinnerComponent } from './shared/loading-spinner.component';
+import { SharedModule } from './shared/shared.module';
 
 
 // declare a name for the array of routes
@@ -21,7 +21,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, HomeComponent, LoadingSpinnerComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent],
   imports: [
     BrowserModule,
     AuthModule,
@@ -29,7 +29,8 @@ const routes: Routes = [
     HttpClientModule,
     appRoutingModule,
     RouterModule.forRoot(routes), // include imports for routes into the main module
-    IconSpriteModule
+    IconSpriteModule,
+    SharedModule
   ],
   exports: [
     RouterModule // include exports for routes from the main module
