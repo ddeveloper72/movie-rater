aa99ef966231b66970ce7332cbe798dcbcd76c5c
ddeveloper72 <duncan.niamh@gmail.com>
2021-01-09 15:12:57 +0000

    updated directive for future WIP

--- src/app/shared/placeholder.directive.ts
+++ src/app/shared/placeholder.directive.ts
@@ -1,10 +1,8 @@
-import { Directive } from '@angular/core';
+import { Directive, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appPlaceholder]'
 })
 export class PlaceholderDirective {
-
-  constructor() { }
-
+  constructor(public viewContainerRef: ViewContainerRef) {}
 }
