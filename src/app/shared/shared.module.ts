79f86014119b918f4286efaa93bbb6e16af19421
ddeveloper72 <duncan.niamh@gmail.com>
2021-01-09 12:21:16 +0000

    placeholder directive added to imports

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component'
+import { LoadingSpinnerComponent } from './loading-spinner/loading-spinner.component';
+import { PlaceholderDirective } from './placeholder.directive'
 
 
 
 @NgModule({
-  declarations: [LoadingSpinnerComponent],
+  declarations: [LoadingSpinnerComponent, PlaceholderDirective],
   imports: [
     CommonModule
   ],
