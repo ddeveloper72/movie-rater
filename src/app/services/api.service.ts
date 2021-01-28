98560ddd761e8754bd935f78c2f9a0ec0ee4a001
ddeveloper72 <duncan.niamh@gmail.com>
2021-01-28 17:32:12 +0000

    removed local environment import

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
-import { environment } from '../../environments/environment';  // used during dev for storing token
+// import { environment } from '../../environments/environment';  // used during dev for storing token
 import { Movie } from '../models/Movie';
 import { User } from '../models/User';
 import { AuthenticationService } from './authentication.service';
