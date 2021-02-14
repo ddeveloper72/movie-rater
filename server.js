224eb6d2f8ece59525221c3c7425ddbe83f95a8b
ddeveloper72 <duncan.niamh@gmail.com>
2021-02-14 22:48:00 +0000

    updated server.js

--- server.js
+++ server.js
@@ -5,12 +5,15 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/<name-on-package.json>'));
+app.use(express.static(__dirname + '/dist/movie-rater'));
 
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/<name-on-package.json>/'}
-);
+app.get('/*', function(req,res) {
+
+res.sendFile(path.join(__dirname+'/dist/movie-rater/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+var port = process.env.PORT || 8080;
+app.listen(port, function() {
+  console.log('Server running on port ' + port + '.');
+})
