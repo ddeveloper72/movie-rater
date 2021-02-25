809aebeddb270adddbb2cd5a54d7c2c32103be18
ddeveloper72 <duncan.niamh@gmail.com>
2021-02-25 00:02:24 +0000

    added  routes for movies, new, movie, update

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,15 +1,28 @@
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule  } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
 import { AuthGuard } from './helper/auth.guard';
+import { MovieFormComponent } from './movies/movie-form/movie-form.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
+import { NgModule } from '@angular/core';
 
-const routes: Routes = [
-    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
-    { path: 'auth', component: AuthComponent },
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'auth', component: AuthComponent },
+  { path: 'movies', component: MovieListComponent },
+  { path: 'movie/:id', component: MovieDetailsComponent },
+  { path: 'new', component: MovieFormComponent },
+  { path: 'update/:id', component: MovieFormComponent },
 
-    // otherwise redirect to home
-    { path: '**', redirectTo: '' }
+  // otherwise redirect to home
+  { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
 
-export const appRoutingModule = RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' });
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
