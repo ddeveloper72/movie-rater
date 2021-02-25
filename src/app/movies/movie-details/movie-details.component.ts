5d33dd3ef9cab0c57abc929d2ca4ba4998f89899
ddeveloper72 <duncan.niamh@gmail.com>
2021-02-25 00:21:42 +0000

    added movie details .ts to new locaton under movies component

---
+++ src/app/movies/movie-details/movie-details.component.ts
@@ -0,0 +1,53 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { Movie } from '../../models/Movie';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-movie-details',
+  templateUrl: './movie-details.component.html',
+  styleUrls: ['./movie-details.component.css']
+})
+export class MovieDetailsComponent implements OnInit {
+  @Input() movie: Movie;
+  @Output() updateMovie = new EventEmitter<Movie>();
+  rateHovered = 0;
+
+  constructor(private apiService: ApiService, private route: ActivatedRoute) {}
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(params => {
+      console.log(params.get('id'));
+      this.apiService.getMovie(params.get('id')).subscribe(c => {
+        console.log(c);
+        this.movie = c;
+      });
+    });
+  }
+
+  rateHover(rate: number): void {
+    this.rateHovered = rate;
+  }
+
+  rateClicked(rate: number): void {
+    this.apiService
+      .rateMovie(rate, this.movie.id)
+      .subscribe(result => this.getDetails(), error => console.log(error));
+  }
+
+  getDetails(): void {
+    this.apiService.getMovie(this.movie.id).subscribe(
+      (movie: Movie) => {
+        this.updateMovie.emit(movie);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  closeMovie(event): void {
+    this.updateMovie.emit();
+  }
+
+}
