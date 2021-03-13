0576e72ef2b73e69aee88b49ff410e278cd57e5d
ddeveloper72 <duncan.niamh@gmail.com>
2021-03-13 09:49:16 +0000

    incoporate check for current user needed for markup nav link

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 // import { Router } from '@angular/router';
-// import { User } from './models/User';
-// import { AuthenticationService } from './services/authentication.service';
+import { User } from './models/User';
+import { AuthenticationService } from './services/authentication.service';
 
 
 @Component({
@@ -10,7 +10,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  // currentUser: User;
+  currentUser: User;
 
-  constructor() {}
+  constructor(public authenticationService: AuthenticationService) {
+    this.authenticationService.currentUser.subscribe(
+      x => (this.currentUser = x)
+    );
+  }
 }
