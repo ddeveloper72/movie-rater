4c35794cb66829ef5c2a63a70dd11131bfb53c42
ddeveloper72 <duncan.niamh@gmail.com>
2021-03-21 02:00:24 +0000

    refactoring appRoutes WIP

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,23 +1,19 @@
 import { Routes, RouterModule  } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
-import { AuthComponent } from './auth/auth.component';
-import { AuthGuard } from './helper/auth.guard';
-import { MovieFormComponent } from './movies/movie-form/movie-form.component';
-import { MovieListComponent } from './movies/movie-list/movie-list.component';
-import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 import { NgModule } from '@angular/core';
+import { MainComponent } from './movies/main.component';
+import { AuthGuard } from './helper/auth.guard';
 
 const appRoutes: Routes = [
-  { path: '', component: HomeComponent},
-  { path: 'auth', component: AuthComponent },
-  { path: 'movies', component: MovieListComponent },
-  { path: 'movie/:id', component: MovieDetailsComponent },
-  { path: 'new', component: MovieFormComponent },
-  { path: 'edit/:id', component: MovieFormComponent },
-
-  // otherwise redirect to home
-  { path: '', redirectTo: '/home', pathMatch: 'full' }
+  // { path: '', component: HomeComponent },
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  {
+    path: 'movies',
+    component: MainComponent,
+    canActivate: [AuthGuard]
+  },
+  { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
