e0e3e2fbf7d8c9d979287eb709496d7e3dd1bdac
ddeveloper72 <duncan.niamh@gmail.com>
2021-12-05 12:24:34 +0000

    test and updated server.js

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/movie-rater'));
-
-app.get('/*', (req, res) =>
-
-res.sendFile('index.html', {root: '/dist/movie-rater/'}),
-);
-
 // Start the app by listening on the default Heroku port
-var port = process.env.PORT || 8080;
+const port = 8080;
+
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/movie-rater'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html', { root: __dirname })
+});
+
+
+app.listen(port, () => {
+  console.log("Server is listening on port " + port);
+});
