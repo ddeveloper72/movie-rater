8635d9ca5d0cbed9281bdbe5c75c06392515393f
Duncan Falconer <duncan.niamh@gmail.com>
2023-08-30 00:06:21 +0100

    refactored routing to suit angular update

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,25 +1,28 @@
-import { Routes, RouterModule  } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
-import { MainComponent } from './movies/main.component';
 import { AuthGuard } from './helper/auth.guard';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { MainComponent } from './movies/main.component';
+
 
 const appRoutes: Routes = [
+  // { path: '', redirectTo: '/movies', pathMatch: 'full' },
   { path: 'welcome', component: HomeComponent },
   { path: '', redirectTo: '/welcome', pathMatch: 'full' },
+  { path: 'auth' , loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
   {
     path: 'movies',
     component: MainComponent,
-    canActivate: [AuthGuard]
+    canActivate: [AuthGuard],
   },
-  { path: '**', component: PageNotFoundComponent }
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(appRoutes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
 })
-
 export class AppRoutingModule {}
