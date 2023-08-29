3ba8c7b138dfe4e9898a56892eb7441fad3f9d9a
Duncan Falconer <duncan.niamh@gmail.com>
2023-08-30 00:32:44 +0100

    separated routes into separate module

--- src/app/movies/main.module.ts
+++ src/app/movies/main.module.ts
@@ -1,7 +1,5 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Routes, RouterModule } from '@angular/router';
-
 import { ApiService } from '../services/api.service';
 import { IconSpriteModule } from 'ng-svg-icon-sprite';
 import { ReactiveFormsModule } from '@angular/forms';
@@ -11,33 +9,28 @@ import { MovieListComponent } from './movie-list/movie-list.component';
 import { MovieFormComponent } from './movie-form/movie-form.component';
 import { MovieDetailsComponent } from './movie-details/movie-details.component';
 import { SharedModule } from '../shared/shared.module';
+import { MainRoutingModule } from './main-routing.module';
+import { RouterModule } from '@angular/router';
 
 // declare a name for the array of routes
-const routes: Routes = [
-  { path: 'movies', component: MovieListComponent },
-  { path: 'movie/:id', component: MovieDetailsComponent },
-  { path: 'new', component: MovieFormComponent },
-  { path: 'edit/:id', component: MovieFormComponent },
 
-  // otherwise redirect to home
-  // { path: '', redirectTo: '/movies', pathMatch: 'full' }
-];
+
 @NgModule({
   declarations: [
     MainComponent,
     MovieListComponent,
     MovieFormComponent,
-    MovieDetailsComponent
+    MovieDetailsComponent,
   ],
   imports: [
+    RouterModule,
     CommonModule,
     // added imports for routes
-    RouterModule.forChild(routes),
     ReactiveFormsModule,
     IconSpriteModule,
-    SharedModule
+    SharedModule,
+    MainRoutingModule
   ],
-  exports: [RouterModule],
-  providers: [ApiService]
+  providers: [ApiService],
 })
 export class MainModule {}
